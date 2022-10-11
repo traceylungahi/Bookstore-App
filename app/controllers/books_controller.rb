b81435209a1a9d4e59eab39c1904c8e7c804fd2d
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-10-11 16:31:29 +0300

    Added index and show methods to book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,13 @@
 class BooksController < ApplicationController
+    def index 
+        books = Book.all 
+        render json: books 
+    end 
+
+    def show 
+        book = find_book
+        render json: book 
+    end 
+
+    def 
 end
