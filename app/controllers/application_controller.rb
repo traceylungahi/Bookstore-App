bfaf845cb5e180345436cea69ad239a5c1bc19d7
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-10-11 17:21:44 +0300

    Updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,18 @@ class ApplicationController < ActionController::API
 
   protect_from_forgery with: :null_session 
 
-  
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  before_action :authorize
+
+  private 
+
+  def authorize
+    @user = User.find_by(id: session[:user_id])
+    return render json: { errors: ["Not authorized"] }, status: :unauthorized unless @user
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
