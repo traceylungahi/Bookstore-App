9cdcbde41e03383021050545b205fd905e0018f3
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-10-11 16:23:30 +0300

    Generated Book model and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  protect_from_forgery with: :null_session 
 end
---
+++ db/migrate/20221011132036_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.string :image
+      t.string :description
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
